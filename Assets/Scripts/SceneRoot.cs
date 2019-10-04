6c4df62cb0a9722518f8819fc1faa5cfa6609cae
Collin <collinclyons@gmail.com>
2019-10-04 16:40:05 -0700

    Initial Scene Switching Logic & Proof of Concept

---
+++ Assets/Scripts/SceneRoot.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameEventListener))]
+public class SceneRoot : MonoBehaviour
+{
+    private List<Transform> props = new List<Transform>();
+
+    //[Header("Scene start/stop game events.")]
+    //public GameEventListener EventListener;
+
+    [SerializeField]
+    private bool sceneIsActive = false;
+
+
+    private void Awake()
+    {
+        GrabAllPropChildren();
+    }
+
+    public void StartScene()
+    {
+        GrowProps();
+    }
+
+    public void StopScene()
+    {
+        ShrinkProps();
+    }
+
+    public void ToggleScene()
+    {
+        if(sceneIsActive)
+        {
+            StopScene();
+        }
+        else
+        {
+            StartScene();
+        }
+
+        sceneIsActive = !sceneIsActive;
+    }
+
+    private void GrowProps()
+    {
+        for (int i = 0; i < props.Count; i++)
+        {
+            Animator anim = props[i].GetComponent<Animator>();
+            anim.speed = Random.Range(0.85f, 1.3f);
+            anim.SetTrigger("grow");
+        }
+    }
+
+    private void ShrinkProps()
+    {
+        for (int i = 0; i < props.Count; i++)
+        {
+            Animator anim = props[i].GetComponent<Animator>();
+            anim.speed = Random.Range(0.95f, 1.4f);
+            anim.SetTrigger("shrink");
+        }
+    }
+
+    private void GrabAllPropChildren()
+    {
+        foreach (Transform c in this.transform)
+        {
+            if(c.CompareTag("Prop"))
+            {
+                props.Add(c);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StoryTeller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TimedGameEvent
+{
+    public GameEvent gameEvent;     // The event that is raised (this can call anything from a scene change to a minor update)
+    public float delayBeforeRaise;  // How long after the previous event is raised (in seconds) before this event is raised as well? 
+    public AudioClip clip;          // May not use this, but could be a good way to trigger each scene's audio instead of having one massive clip.
+}
+
+//TODO: Consider resetting game timer to 0 every time an event is called? Not sure yet.
+
+public class StoryTeller : MonoBehaviour
+{
+    public List<TimedGameEvent> StoryEvents;
+
+    private AudioSource storytellerAudioSource;
+
+    [SerializeField]
+    private float gameTimer = 0.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        storytellerAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer += Time.deltaTime;
+
+        if(StoryEvents.Count > 0)
+        {
+            if(gameTimer >= StoryEvents[0].delayBeforeRaise)
+            {
+                RaiseTimedGameEvent(0);
+            }
+        }
+    }
+
+    private void RaiseTimedGameEvent(int index)
+    {
+        TimedGameEvent e = StoryEvents[index];
+        e.gameEvent.Raise();
+        // e.clip.Play();
+        if(e.clip != null)
+            storytellerAudioSource.PlayOneShot(e.clip);
+
+        gameTimer = 0.0f;
+        StoryEvents.RemoveAt(index);
+        
+    }
+}
